b23472b354f1cece9343e852d625db53d807e81e
Abel <amoncada24@students.bmchs.org>
2022-01-24 08:56:20 -0600

    updated

---
+++ project_1_mad_libs.py
@@ -0,0 +1,12 @@
+print("A Walk In The Park: A Mad Lib")
+print("Instructions: You will be asked for a type of word. \nAnswer each correctly and the story will be written when you are finished.")
+placeholder1 = input
+placeholder2 = input
+placeholder3 = input
+placeholder4 = input
+placeholder5 = input
+placeholder6 = input
+placeholder7 = input
+placeholder8 = input
+placeholder9 = input
+placeholder10 = input
